ec768ae0f0ba56fe4bd30caf9f325c70a00e3a4e
gemaymini <446914334@qq.com>
2026-01-06 21:47:57 +0800

    优化config

--- src/configuration/config.py
+++ src/configuration/config.py
@@ -16,16 +16,16 @@ class Config:
     
     # ==================== 进化算法参数 ====================
     POPULATION_SIZE =100         # 种群大小 (Aging Evolution Queue Size)
-    MAX_GEN = 100                 # 最大进化代数 (Total number of individuals to evaluate in search)
+    MAX_GEN = 5000                 # 最大进化代数 (Total number of individuals to evaluate in search)
     TOURNAMENT_SIZE = 5            # 锦标赛选择的样本大小 (Sample Size)
     TOURNAMENT_WINNERS = 2          # 锦标赛选择的胜者数量 (Parent Size)
     
     # ==================== 筛选与训练流程参数 ====================
-    HISTORY_TOP_N1 = 5             # 第一轮筛选：基于NTK选择Top N1
-    SHORT_TRAIN_EPOCHS = 10         # 第一轮筛选：短期训练轮数
+    HISTORY_TOP_N1 = 10             # 第一轮筛选：基于NTK选择Top N1
+    SHORT_TRAIN_EPOCHS = 30         # 第一轮筛选：短期训练轮数
     
-    HISTORY_TOP_N2 =  1             # 第二轮筛选：基于验证集Acc选择Top N2
-    FULL_TRAIN_EPOCHS = 50         # 最终训练：完整训练轮数
+    HISTORY_TOP_N2 =  3             # 第二轮筛选：基于验证集Acc选择Top N2
+    FULL_TRAIN_EPOCHS = 500         # 最终训练：完整训练轮数
     
     # ==================== 交叉算子参数 ====================
     PROB_CROSSOVER = 0.5            # 交叉概率 (0.8)
@@ -92,7 +92,7 @@ class Config:
     
     # ==================== 训练参数 ====================
     DEVICE = 'cuda'                 
-    BATCH_SIZE = 128             
+    BATCH_SIZE = 256             
     LEARNING_RATE = 0.1             
     MOMENTUM = 0.9                  
     WEIGHT_DECAY = 5e-4
@@ -104,7 +104,7 @@ class Config:
     
     # ==================== ImageNet 专用参数 ====================
     IMAGENET_ROOT = os.path.join(_PROJECT_ROOT, 'data', 'imagenet')  # ImageNet 数据集根目录
-    IMAGENET_BATCH_SIZE =512           # ImageNet 批次大小（显存考虑）
+    IMAGENET_BATCH_SIZE = 256          # ImageNet 批次大小（显存考虑）
     IMAGENET_INPUT_SIZE = 224          # ImageNet 输入尺寸
     IMAGENET_NUM_CLASSES = 1000        # ImageNet 类别数
     IMAGENET_LR = 0.1                  # ImageNet 初始学习率
